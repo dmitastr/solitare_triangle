c0a25f549b2e8010421f8bb125a2c19961b7e478
dmitastr <43439336+dmitastr@users.noreply.github.com>
2019-07-01 00:28:55 +0300

    Add files via upload
    1. Fixed game logic
    2. Add reset button
    3. Add moves counter
    4. Add winning condition

---
+++ pin.js
@@ -0,0 +1,59 @@
+class Pin {
+    constructor(x, y, r, row, idx) {
+        this.x = x;
+        this.y = y;
+        this.r = r;
+        this.empty = false;
+        this.active = false;
+        this.colorNotEmpty = 50;
+        this.colorEmpty = 255;
+        this.borderNotActive = 0;
+        this.borderActive = color(255, 204, 0);
+        this.row = row;
+        this.idx = idx;
+    }
+
+    show() {
+        if (this.active) {
+            stroke(this.borderActive);
+        } else {
+            stroke(this.borderNotActive);
+        }
+        if (this.empty) {
+            fill(this.colorEmpty);
+        } else {
+            fill(this.colorNotEmpty);
+        }
+        strokeWeight(5);
+        ellipse(this.x, this.y, this.r);
+        // textAlign(CENTER, CENTER);
+        // fill(200);
+        // stroke(this.borderNotActive);
+        // text(`${this.row}, ${this.idx}`, this.x, this.y);
+    }
+
+    select() {
+        this.active = !this.active;
+    }
+
+    isNeighbor(other) {
+        if (this.row===other.row) {
+            if (Math.abs(this.idx - other.idx)<=2) {
+                return this.idx - other.idx;
+            } else {
+                return 0;
+            }
+        } else {
+            if (Math.abs(this.row-other.row)<=2 && ((this.idx===other.idx) || (this.idx-other.idx)===(this.row-other.row))) {
+                return this.row-other.row;
+            } else {
+                return 0;
+            }
+        }
+
+    }
+
+
+
+
+}
---
+++ sketch.js
@@ -0,0 +1,128 @@
+let pin;
+let pins;
+const n = 3; //number of pins
+const rows = 5; //number of rows
+let rowsTmp;
+let row;
+const x = 400;
+const y = 50;
+const r = 70;
+let firstMove;
+let hasSelected;
+const rowsLookup = {1:1, 2:2, 3:2, 4:3, 5:3, 6:3, 7:4, 8:4, 9:4, 10:4, 11:5, 12:5, 13:5, 14:5, 15:5};
+const isOdd = (num) => num%2!=0;
+let pinsToDo;
+let movesCount = 0;
+
+function setup() {
+    createCanvas(800, 500);
+    resetSketch();
+    let button = createButton("reset");
+    button.position(19, 19);
+    button.mousePressed(resetSketch);
+}
+
+function draw() {
+    background(255);
+    for (let i=0; i<pins.length; i++) {
+        pins[i].show();
+    }
+    textAlign(LEFT, LEFT);
+    strokeWeight(1);
+    fill(0);
+    textSize(14);
+    text(`Moves count\n${movesCount}`, 19, 80);
+    if (pinsToDo<=12) {
+        textAlign(CENTER, CENTER);
+        fill(color(200, 0, 0));
+        strokeWeight(3);
+        stroke(0);
+        textSize(40);
+        text('You are winner!', width/2, height/2);
+    }
+}
+
+function mouseClicked() {
+    for (let i=0; i<pins.length; i++) {
+        let pin = pins[i];
+        if (Math.abs(mouseX-pin.x)<=pin.r/2 && Math.abs(mouseY-pin.y)<=pin.r/2) {
+            if (firstMove) {
+                pin.empty = true;
+                firstMove = false;
+            } else if (!pin.empty || hasSelected>=0) {
+                if (!pin.active) {
+                    if (hasSelected>=0 && i!=hasSelected) {
+                        pins[hasSelected].select();
+                        pin.select();
+                        let pinsClose = pin.isNeighbor(pins[hasSelected]);
+                        if (Math.abs(pinsClose)===2) {
+                            let row = (pins[hasSelected].row - pin.row)/2;
+                            let idx = (pins[hasSelected].idx - pin.idx)/2;
+                            // console.log(row, idx);
+                            let pinMid = findByRowIdx(pins[hasSelected].row-row, pins[hasSelected].idx-idx);
+                            // console.log(pinMid);
+                            // console.log("pins are close", pinsClose);
+                            if (pinMid && !pinMid.empty && pin.empty && !pins[hasSelected].empty) {
+                                console.log("empty pin", pinMid.row, pinMid.idx);
+                                pinMid.empty = true;
+                                pin.empty = false;
+                                pins[hasSelected].empty = true;
+                                pinsToDo -= 1;
+                                movesCount += 1;
+                            }
+                        }
+                        hasSelected = i;
+                    } else {
+                        hasSelected = i;
+                        pin.select();
+                    }
+                    
+                } else {
+                    pin.select();
+                    hasSelected = -1;
+                }
+        console.log(hasSelected);
+                    
+            }
+        }
+    }    
+}
+
+function findByRowIdx(row, idx) {
+    const pin = pins.filter(pin => pin.row===row && pin.idx===idx);
+    if (pin.length>0) {
+        return pin[0];
+    } else {
+        return undefined;
+    }
+}
+
+function resetSketch() {
+    pins = [];
+    firstMove = true;
+    hasSelected = -1;
+    rowsTmp = [];
+    movesCount = 0;
+    for (let i=0; i<rows; i++) {
+        if (i===0) {
+            pin = new Pin(x, y, r, 0, 0);
+            pins.push(pin);
+            rowsTmp.push(pin);
+        } else {
+            row = [];
+            rowsTmp.map((pinTmp, idx) => {
+                pin = new Pin(pinTmp.x-r*0.8, pinTmp.y+r*1.2, r, i, idx);
+                row.push(pin);
+                pins.push(pin);
+                if (idx===rowsTmp.length-1) {
+                    pin = new Pin(pinTmp.x+r*0.8, pinTmp.y+r*1.2, r, i, idx+1);
+                    row.push(pin);
+                    pins.push(pin);
+                }
+            });
+            rowsTmp = row;
+        }
+    }
+    console.log(pins.length);
+    pinsToDo = pins.length - 1;
+}
